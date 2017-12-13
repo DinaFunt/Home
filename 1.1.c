dfe499a8caaf4eb5113637178a9e8a97c2f7d0e6
Dina Funt <dina.funt@gmail.com>
2017-12-13 10:50:51 +0300

    slightly modified

---
+++ 1.1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	char c, d;
+	char str[100];
+	int i = 0;
+	printf("Input one symbol\n");
+	scanf_s("%c", &c, 1);
+	printf("Input your string\n");
+	scanf_s("%s", str, 100);
+	while (d = str[i++])
+	{
+	    if (d != c)
+	    {
+			printf("%c", d);
+        }
+	}
+	printf("\n");
+	return 0;
+}
