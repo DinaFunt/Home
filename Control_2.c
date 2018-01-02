41c24f186101913126de1b4e30cb32dca48ddd1d
Dina Funt <dina.funt@gmail.com>
2018-01-02 10:18:24 +0300

    modified #2

--- Control_2.c
+++ Control_2.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 #include <string.h>
+#define MaxSize 20
+
+void number(int n, char* a, unsigned long long *num) {
+    unsigned long long t = 1;
+    while (n >= 0)
+    {
+        *num = *num + ((unsigned long long)(a[n] - '0') * t);
+        t = t * 2;
+        n--;
+    }
+    return;
+}
 
 int main() {
-    char a[50], b[50];
-    int i = 0, n1, n2, fir = 0, sec = 0, t = 1;
-    for (i = 0; i < 50; i++)
+    char a[MaxSize], b[MaxSize];
+    int i = 0, n1, n2;
+    unsigned long long fir = 0, sec = 0;
+    for (i = 0; i < MaxSize; i++)
     {
         a[i] = '0';
         b[i] = '0';
     }
-    printf("Two sequence of 1 and 0\n");
+    printf("Two sequence of 1 and 0, lenght <= 20 \n");
     printf("(The elder digits are in the elements with the least indexes)\n");
     scanf("%s %s", a, b);
     n1 = strlen(a) - 1;
     n2 = strlen(b) - 1;
-    printf("%d %d\n", n1 + 1, n2 + 1);
-    i = 0;
-    while (n1 >= 0)
-    {
-        fir = fir + ((a[n1] - '0') * t);
-        t = t * 2;
-        n1--;
-    }
-    t = 1;
-    while (n2 >= 0)
-    {
-        sec = sec + ((b[n2] - '0') * t);
-        t = t * 2;
-        n2--;
-    }
-    printf("%d %d\n", fir, sec);
+    //printf("%d %d\n", n1 + 1, n2 + 1);
+    number(n1, a, &fir);
+    number(n2, b, &sec);
+    //printf("%d ", fir);
+    //printf("%d\n", sec);
     if (fir > sec)
     {
         printf("first > second\n");
